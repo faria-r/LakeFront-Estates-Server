99549f576d2e20cc9d65d053ffac98bdef259d65
faria-r <fariacse11@gmail.com>
2024-05-01 20:19:57 +0600

    API created for post and get users favourites listing

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 require("dotenv").config();
-const jwt = require('jsonwebtoken')
+const jwt = require("jsonwebtoken");
 const port = process.env.PORT || 5000;
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 
@@ -35,105 +35,126 @@ async function run() {
     const testimonialsCollection = client
       .db("LakeFront-Estates")
       .collection("testimonials");
+    const favouritesCollection = client
+      .db("LakeFront-Estates")
+      .collection("favourites");
 
+    //JWT Token
+    app.post("/jwt", async (req, res) => {
+      const user = req.body;
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+        expiresIn: "3h",
+      });
+      console.log(token);
+      res.send({ token });
+    });
+    //verify Middlewares-jwt
+    const verifyToken = (req, res, next) => {
+      console.log("inside verify token", req.headers.authorization);
+      if (!req.headers.authorization) {
+        return res.status(401).send({ message: "unauthorized access" });
+      }
+      const token = req.headers.authorization.split(" ")[1];
+      jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+        if (err) {
+          return res.status(401).send({ message: "unauthorized access" });
+        }
+        req.decoded = decoded;
+        next();
+      });
+    };
+    //verify admin
 
-      //JWT Token
-      app.post('/jwt',async(req,res)=>{
-        const user = req.body;
-        const token = jwt.sign(user,process.env.ACCESS_TOKEN_SECRET,{
-          expiresIn:'1h'
-        });
-        res.send({token})
-      })  
-//verify Middlewares-jwt
-const verifyToken = (req,res,next) =>{
-  console.log('inside verify token',req.headers);
-  if(!req.headers.authorization){
-    return res.status(401).send({message:'forbidden access'})
-  }
-  const token = req.headers.authorization.split(' ')[1];
-  jwt.verify(token,process.env.ACCESS_TOKEN_SECRET,(error,decoded)=>{
-    if(error){
-      return res.status(401).send({message:'forbidden access'})
-    }
-    req.decoded = decoded;
-    next();
-  })
- 
-  next()
-}
-//verify admin
+    const verifyAdmin = async (req, res, next) => {
+      const email = req.decoded.email;
+      const query = { email: email };
+      const user = await usersCollection.findOne(query);
+      const isAdmin = user?.role === "admin";
+      if (!isAdmin) {
+        return res.status(403).send({ message: "forbidden access" });
+      }
+      next();
+    };
 
-const verifyAdmin = async (req,res,next) =>{
-  const email = req.decoded.email;
-  const query = {email:email};
-  const user = await usersCollection.findOne(query);
-  const isAdmin = user?.role === 'admin';
-  if(!isAdmin){
-    return res.status(403).send({message:'forbidden'})
-  }
-  next();
+    //admin
+    app.get("/user/admin/:email", verifyToken, async (req, res) => {
+      const email = req.params.email;
 
-}
+      if (email !== req.decoded.email) {
+        return res.status(403).send({ message: "forbidden access" });
+      }
 
-
-
-
-//admin
-app.get('/user/admin/:email',verifyToken,async(req,res)=>{
-const email = req.params.email;
-if(email !== req.decoded.email){
-  return res.status(403).send({message:'unauthorized access'})
-}
-const query = {email:email};
-const user = await usersCollection.findOne(query)
-let admin = false;
-if(user){
-  admin = user?.role === 'admin'
-}
-res.send({admin});
-})
-
+      const query = { email: email };
+      const user = await usersCollection.findOne(query);
+      let admin = false;
+      if (user) {
+        admin = user?.role === "admin";
+      }
+      res.send({ admin });
+    });
 
     //API to store all user data in database
     app.post("/users", async (req, res) => {
       const user = req.body;
       //insert user if they doesnt exist
       //many ways for this(1.email.unique, 2.upsert,3.simple checking)
-      const query= {email:user.email}
+      const query = { email: user.email };
       const existingUser = await usersCollection.findOne(query);
-      if(existingUser){
-        return res.send({message:'user already exist',insertedId:null})
+      if (existingUser) {
+        return res.send({ message: "user already exist", insertedId: null });
       }
       const result = await usersCollection.insertOne(user);
       res.send(result);
     });
-//API to get all users
-app.get('/users', verifyToken,verifyAdmin,  async(req,res)=>{
-  console.log(req.headers)
-const query = {};
-const result = await usersCollection.find(query).toArray();
-res.send(result)
-})
-//delete a user
-app.delete('/users/:id',verifyToken,verifyAdmin,async(req,res)=>{
-  const id = req.params.id;
-  const query={_id: new ObjectId(id)}
-  const result = await usersCollection.deleteOne(query);
-  res.send(result)
-})
-//
-app.patch('/users/admin/:id',verifyToken,verifyAdmin,async(req,res)=>{
-  const id = req.params.id;
-  const filter = {_id:new ObjectId(id)}
-  const updatedDoc = {
-    $set:{
-      role:'admin'
-    }
-  }
-  const result = await usersCollection.updateOne(filter,updatedDoc)
-res.send(result)
-})
+    //API to store users favourites
+    app.post('/favourites', async(req,res)=>{
+      const favourites = req.body;
+      const query = {homeID : favourites.homeID,};
+      const existingHome = await favouritesCollection.findOne(query);
+      if(existingHome){
+        return res.send({message:'already added to favourites', insertedId:null});
+      }
+      const result = await favouritesCollection.insertOne(favourites);
+      res.send(result)
+    })
+    //API to get favourites based on users
+    app.get('/favourites/:email', async (req,res)=>{
+      const email = req.params.email;
+      const query = {userEmail:email}
+      const result = await favouritesCollection.find(query).toArray();
+      res.send(result)
+    })
+    //API to get all users
+    app.get("/users", verifyToken, verifyAdmin, async (req, res) => {
+      console.log(req.headers);
+      const query = {};
+      const result = await usersCollection.find(query).toArray();
+      res.send(result);
+    });
+    //delete a user
+    app.delete("/users/:id", verifyToken, verifyAdmin, async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await usersCollection.deleteOne(query);
+      res.send(result);
+    });
+    //
+    app.patch(
+      "/users/admin/:id",
+      verifyToken,
+      verifyAdmin,
+      async (req, res) => {
+        const id = req.params.id;
+        const filter = { _id: new ObjectId(id) };
+        const updatedDoc = {
+          $set: {
+            role: "admin",
+          },
+        };
+        const result = await usersCollection.updateOne(filter, updatedDoc);
+        res.send(result);
+      }
+    );
     //api to get all homelist
     app.get("/homeList", async (req, res) => {
       const query = {};
